a62fc67df574293a0e1107834f66ee7e247c1f58
Darius Opritoiu <dariusopritoiu123@gmail.com>
2021-07-22 12:14:21 +0300

    implemented get method in classdao

--- src/main/java/ibm/practica/checkin/dao/ClassDao.java
+++ src/main/java/ibm/practica/checkin/dao/ClassDao.java
@@ -2,18 +2,29 @@ package ibm.practica.checkin.dao;
 
 import ibm.practica.checkin.entity.Class;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 import java.util.Optional;
+import java.util.Queue;
 
 public class ClassDao implements Dao<Class>{
+
+    private final EntityManager entityManager;
+
+    public ClassDao(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+dsa
     @Override
     public Optional<Class> get(long id) {
-        return Optional.empty();
+        return Optional.ofNullable(entityManager.find(Class.class,id));
     }
 
     @Override
     public List<Class> getAll() {
-        return null;
+        Query query = entityManager.createQuery("SELECT e FROM Class e");
+        return query.getResultList();
     }
 
     @Override
