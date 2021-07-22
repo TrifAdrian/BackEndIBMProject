e98967cfbc8e1b2bde50aeed82c16badeadca837
Darius Opritoiu <dariusopritoiu123@gmail.com>
2021-07-22 13:25:43 +0300

    implemented classdao and classroomdao except update

--- src/main/java/ibm/practica/checkin/dao/ClassDao.java
+++ src/main/java/ibm/practica/checkin/dao/ClassDao.java
@@ -2,11 +2,17 @@ package ibm.practica.checkin.dao;
 
 import ibm.practica.checkin.entity.Class;
 
+
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Query;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
-import java.util.Queue;
+
+import java.util.function.Consumer;
+
+import static java.lang.Integer.parseInt;
 
 public class ClassDao implements Dao<Class>{
 
@@ -15,7 +21,7 @@ public class ClassDao implements Dao<Class>{
     public ClassDao(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
-dsa
+
     @Override
     public Optional<Class> get(long id) {
         return Optional.ofNullable(entityManager.find(Class.class,id));
@@ -29,16 +35,42 @@ dsa
 
     @Override
     public void save(Class aClass) {
-
+        executeInsideTransaction(entityManager1 -> entityManager.persist(aClass));
     }
 
     @Override
     public void update(Class aClass, String[] params) {
 
+        aClass.setName(Objects.requireNonNull(params[0],"Name cannot be null"));
+
+        aClass.setYear(Objects.requireNonNull(parseInt(params[1]),"Year cannot be null"));
+
+        aClass.setSection(Objects.requireNonNull(params[2],"Section cannot be null"));
+
+//        aClass.setTeacher(Objects.requireNonNull(parseInt(params[3]),"Teacher cannot be null"));
+//
+//        aClass.setStudentList(Objects.requireNonNull(parseInt(params[4]),"List cannot be null"));
+//
+//        aClass.setClassroom(Objects.requireNonNull(parseInt(params[5]),""));
+
+        executeInsideTransaction(entityManager -> entityManager.merge(aClass));
     }
 
     @Override
     public void delete(Class aClass) {
+        executeInsideTransaction(entityManager ->entityManager.remove(aClass));
+    }
 
+    private void executeInsideTransaction(Consumer<EntityManager> action) {
+        final EntityTransaction tx = entityManager.getTransaction();
+        try {
+            tx.begin();
+            action.accept(entityManager);
+            tx.commit();
+        }
+        catch (RuntimeException e) {
+            tx.rollback();
+            throw e;
+        }
     }
 }
--- src/main/java/ibm/practica/checkin/dao/ClassroomDao.java
+++ src/main/java/ibm/practica/checkin/dao/ClassroomDao.java
@@ -2,23 +2,35 @@ package ibm.practica.checkin.dao;
 
 import ibm.practica.checkin.entity.Classroom;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 public class ClassroomDao implements Dao<Classroom>{
+    private final EntityManager entityManager;
+
+    public ClassroomDao(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
     @Override
     public Optional<Classroom> get(long id) {
-        return Optional.empty();
+        return Optional.ofNullable(entityManager.find(Classroom.class,id));
     }
 
     @Override
     public List<Classroom> getAll() {
-        return null;
+        Query query = entityManager.createQuery("select e from Classroom e");
+        return query.getResultList();
     }
 
     @Override
     public void save(Classroom classroom) {
-
+        executeInsideTransaction(entityManager -> entityManager.persist(classroom));
     }
 
     @Override
@@ -28,6 +40,19 @@ public class ClassroomDao implements Dao<Classroom>{
 
     @Override
     public void delete(Classroom classroom) {
+        executeInsideTransaction(entityManager -> entityManager.remove(classroom) );
+    }
 
+    private void executeInsideTransaction(Consumer<EntityManager> action) {
+        final EntityTransaction tx = entityManager.getTransaction();
+        try {
+            tx.begin();
+            action.accept(entityManager);
+            tx.commit();
+        }
+        catch (RuntimeException e) {
+            tx.rollback();
+            throw e;
+        }
     }
 }
