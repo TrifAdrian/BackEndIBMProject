73638b284fe3a40ddbbafe5eb29eaaf349687e24
AdrianTrif <adrian.trif00@e-uvt.ro>
2021-07-28 17:16:47 +0300

    Restructured folders v2

--- src/main/java/ibm/practica/checkin/db/model/Class.java
+++ src/main/java/ibm/practica/checkin/db/model/Class.java
@@ -2,6 +2,7 @@ package ibm.practica.checkin.db.model;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -108,4 +109,42 @@ public class Class {
         this.section = section;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Class other = (Class) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Class{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", year=" + year +
+                ", section='" + section + '\'' +
+                ", teacher=" + teacher +
+                ", users=" + users +
+                ", classrooms=" + classrooms +
+                ", classDates=" + classDates +
+                '}';
+    }
 }
